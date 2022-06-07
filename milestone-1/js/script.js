3bdd0ac2712cdec1ae5689013c895e5dfd469fb1
kaijie26 <chenkaijie26@gmail.com>
2022-06-07 17:42:10 +0200

    fix milestone 1

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -1,13 +1,20 @@
 // 1.SCIVO I NUMERI DA 1 A 100
 for(let i = 1; i <= 100; i++) {
     if(i % 5 === 0 && i % 3 === 0){
-        i = `fizzbuzz`
+        let fizzBuzz = 'fizzbuzz'
+       //Stampo i fizzbuzz
+        console.log(fizzBuzz);
     }else if (i % 5 === 0){
-        i = `buzz`
+        let buzz = 'buzz'
+        //Stampo i buzz
+        console.log(buzz);
     }else if (i % 3 === 0){
-        i = `fizz`
+        let fizz = 'fizz'
+        //Stampo i fizz
+        console.log(fizz);
+    }else{
+        //Stampo i numeri 
+        console.log(i);
     }
 
-    console.log(i);
-    
 }
